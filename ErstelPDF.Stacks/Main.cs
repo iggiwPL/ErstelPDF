8b4f0078552050b50d8896c50f1e13544445c510
iggiwPL <63921487+iggiwPL@users.noreply.github.com>
2025-04-13 22:39:33 +0200

    Add files via upload

---
+++ ErstelPDF.Stacks/Main.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ErstelPDF.Stacks
+{
+    public static class ErstelStacks
+    {
+        static Stack<String> document = new Stack<String>();
+
+    }
+    internal static class ErstelStacks_Variables
+    {
+
+    }
+}
