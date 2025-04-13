984be6db4d7979c56ce9d377e4360e80eeade884
iggiwPL <63921487+iggiwPL@users.noreply.github.com>
2025-04-13 22:38:57 +0200

    Add files via upload

---
+++ ErstelPDF.Core/Core.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using ErstelPDF.Stacks;
+
+namespace ErstelPDF.Core
+{
+    public class ErstelCore
+    {
+        public void CreateEmptyFile(string path)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Create(path)))
+            {
+                writer.WriteLine("%PDF-1.0");
+            }
+        }
+        
+    }
+
+
+
+
+    // Used for header of the PDF
+    internal static class BinaryWriter_Extension
+    {
+        public static void WriteLine(this BinaryWriter writer,string text)
+        {
+            writer.Write(text);
+            writer.Write('\n'); // Test
+        }
+    }
+
+    
+}
