52cbbc4da430d7c606e41fc77562eb1a823b8917
iggiwPL <63921487+iggiwPL@users.noreply.github.com>
2025-04-16 15:10:27 +0200

    Add files via upload

--- ErstelPDF.Core/Core.cs
+++ ErstelPDF.Core/Core.cs
@@ -1,16 +1,19 @@
 ﻿using System.IO;
 using System.Runtime.CompilerServices;
 using ErstelPDF.Stacks;
+using ErstelPDF.Dictionary;
 
 namespace ErstelPDF.Core
 {
     public class ErstelCore
     {
+        DictionaryPDF dictionaryPDF = new DictionaryPDF();
+
         public void CreateEmptyFile(string path)
         {
             using (BinaryWriter writer = new BinaryWriter(File.Create(path)))
             {
-                writer.WriteLine("%PDF-1.0");
+                writer.WriteLine(dictionaryPDF.GetHeaderPDF());
             }
         }
         
