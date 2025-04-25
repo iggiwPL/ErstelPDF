2519ab82cc4ec57b1cf0e600f1dc21cd3262e7d2
iggiwPL <63921487+iggiwPL@users.noreply.github.com>
2025-04-25 18:57:48 +0200

    Add files via upload

---
+++ ErstelPDF/ErstelPDF.Core/BinaryWriterExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace ErstelPDF.Core
+{
+    // Used for content of the PDF
+    public static class BinaryWriterExtension
+    {
+        public static void WriteStringAsASCII(this BinaryWriter writer, string text)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(text);
+            writer.Write(bytes);
+        }
+    }
+}
---
+++ ErstelPDF/ErstelPDF.Core/ByteCounter.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace ErstelPDF.Core
+{
+    // Used for generating cross reference table and trailer
+    public class ByteCounter
+    {
+        public int CountBytesObject(string content)
+        {
+            return Encoding.ASCII.GetBytes(content + "\n").Length;
+        }
+    }
+}
