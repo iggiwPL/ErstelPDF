6b64c40c00736b9fe521e36a6edc836f74f41188
iggiwPL <63921487+iggiwPL@users.noreply.github.com>
2025-05-09 21:50:42 +0200

    Add files via upload

---
+++ ErstelPDF/ErstelPDF.Transforms/TrailerTransformer.cs
@@ -0,0 +1,28 @@
+﻿using ErstelPDF.Core;
+using ErstelPDF.DataTypes;
+
+namespace ErstelPDF.Transforms
+{
+    public class TrailerTransformer
+    {
+        ByteCounter _byteCounter;
+        private static int XrefByteBegin = 0;
+        public TrailerTransformer()
+        {
+            _byteCounter = new ByteCounter();
+        }
+        public int XrefByteBeginProperty
+        {
+            get { return XrefByteBegin; }
+            set { XrefByteBegin = value; }
+        }
+
+        public void Transform(Queue<LinkedDocumentType> PDFObjects)
+        {
+            foreach (var PDFObject in PDFObjects)
+            {
+                XrefByteBegin += _byteCounter.CountBytesObject(PDFObject.Content);
+            }
+        }
+    }
+}
