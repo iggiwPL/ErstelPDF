0a45ec1e294ad0c3e10c00248e5606b814008fb7
iggiwPL <63921487+iggiwPL@users.noreply.github.com>
2025-05-31 17:25:47 +0200

    Add files via upload

---
+++ ErstelPDF/ErstelPDF.Transforms/Interfaces/ITrailerTransformer.cs
@@ -0,0 +1,16 @@
+﻿using ErstelPDF.Core;
+using ErstelPDF.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErstelPDF.Transforms
+{
+    public interface ITrailerTransformer
+    {
+        public int XrefByteBeginProperty { get; set; }
+        public void Transform(IByteCounter IbyteCounter, Queue<LinkedDocumentType> PDFObjects);
+    }
+}
