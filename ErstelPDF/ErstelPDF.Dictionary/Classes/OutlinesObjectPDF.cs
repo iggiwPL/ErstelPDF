a6c177e07734c845acca5770f1788f267be96321
iggiwPL <63921487+iggiwPL@users.noreply.github.com>
2025-06-01 21:10:05 +0200

    Add files via upload

---
+++ ErstelPDF/ErstelPDF.Dictionary/Classes/OutlinesObjectPDF.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErstelPDF.Dictionary
+{
+    public class OutlinesObjectPDF
+    {
+
+        // To rewrite
+
+        public string GetOutlinesObject(ref int objectID)
+        {
+            int outlinesID = objectID;
+
+            string template = $"{outlinesID} 0 obj\n" +
+                    "<<\n" +
+                    "/Type /Outlines\n" +
+                    "/Count 0\n" +
+                    ">>\n" +
+                    "endobj\n";
+
+            objectID++;
+            return template;
+        }
+
+    }
+}
---
+++ ErstelPDF/ErstelPDF.Stacks/Classes/ErstelStacks.cs
@@ -0,0 +1,13 @@
+﻿using ErstelPDF.DataTypes;
+
+namespace ErstelPDF.Stacks
+{
+    
+    public class ErstelStacks
+    {
+        public Queue<LinkedDocumentType> DocumentTextContent = new Queue<LinkedDocumentType>();
+        
+        public Queue<XReferenceType> XreferenceTable = new Queue<XReferenceType>();
+
+    }
+}
