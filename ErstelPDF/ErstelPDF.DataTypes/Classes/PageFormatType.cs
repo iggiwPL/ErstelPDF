62b1164254e8f9aa9e958dcad50378cf7f2c81e3
iggiwPL <63921487+iggiwPL@users.noreply.github.com>
2025-06-24 13:58:56 +0200

    Add files via upload

---
+++ ErstelPDF/ErstelPDF.DataTypes/Classes/PageFormatType.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErstelPDF.DataTypes
+{
+    public class PageFormatType
+    {
+        public int x_lower { get; set; }
+        public int y_lower { get; set; }
+        public int x_upper { get; set; }
+        public int y_upper { get; set; }
+       
+
+
+        public PageFormatType() { }
+
+        public PageFormatType(int x_lower, int y_lower, int x_upper, int y_upper)
+        {
+            this.x_lower = x_lower;
+            this.y_lower = y_lower;
+
+            this.x_upper = x_upper;
+            this.y_upper = y_upper;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is PageFormatType))
+            {
+                return false;
+            }
+            PageFormatType other = (PageFormatType)obj;
+            return this.x_lower == other.x_lower && this.y_lower == other.y_lower && this.x_upper == other.x_upper && this.y_upper == other.y_upper;
+        }
+        public override int GetHashCode()
+        {
+            return x_lower.GetHashCode() ^ y_lower.GetHashCode() ^ x_upper.GetHashCode() ^ y_upper.GetHashCode();
+        }
+    }
+}
---
+++ ErstelPDF/ErstelPDF.Dictionary/Classes/PageObjectPDF.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErstelPDF.Dictionary
+{
+    public class PageObjectPDF : IPageObjectPDF
+    {
+        // Arguments
+        public int ObjectCounter { get; set; }
+
+        // Active processed
+        public int PagesObject { get; set; }
+        public int PageObject { get; set; }
+
+        // Cache
+        public int SyncCounterTransition { get; set; }
+
+        public PageObjectPDF(int CurrentObject)
+        {
+            this.ObjectCounter = CurrentObject;
+        }
+        public void CountObjects()
+        {
+            this.PagesObject = this.ObjectCounter - 1;
+            this.PageObject = this.ObjectCounter;
+        }
+        public void SyncMainCounter(ref int MainCounter)
+        {
+            this.SyncCounterTransition = this.ObjectCounter + 1;
+            MainCounter = this.SyncCounterTransition;
+        }
+        // Datatype custom is requied for format
+        public string GetObject()
+        {
+            string template = $"{this.PageObject} 0 obj\n" +
+                              "<<\n" +
+                              $"/Type /Page /Parent {this.PagesObject} 0 R /MediaBox [0 0 612 792]\n" +
+                              ">>\n" +
+                             "endobj\n";
+            return template;
+        }
+    }
+}
